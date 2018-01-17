bd152c3f634f4a44a2329f051bf3d28c4084cd32
Dimaa_ <dimaa@mail.de>
2018-01-17 13:06:52 +0100

    Tür hinzugefügt
    DIE CYKA BLYAT TÜR!!!!

---
+++ Giteinstieg Tür und Angel/src/türen/BlyatTür.java
@@ -0,0 +1,15 @@
+package türen;
+
+public class BlyatTür 
+{
+	// Unzerstörbare Tür
+	public BlyatTür() 
+	{
+		
+	}
+	
+	public void zerstörenTür() 
+	{
+		System.out.println("Die Blyat Tür kann man nicht zerstören");
+	}
+}
